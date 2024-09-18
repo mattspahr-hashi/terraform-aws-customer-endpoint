df90428ec8e3881d8e7dbb9d18325cdcb45d4374
Matt Spahr <matt.spahr@matt.spahr-M4MGYYK3WC>
2024-09-18 14:45:22 -0500

    changed variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "instance_type" {
   type        = string
-  default     = "t2.micro"
   description = "Instance Type for Customer Endpoint"
 }
 
 variable "customer_name" {
   type        = string
-  default     = "t2.micro"
   description = "Customer Name"
 }
